736c863afb496e109a38f88045c848c75aba686a
caian <caianpedroso@gmail.com>
2023-04-29 01:19:52 -0300

    add todo

--- app/add-new-todo.js
+++ app/add-new-todo.js
@@ -1,8 +1,32 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+async function addTodo(name, refresh, reSetName) {
+  await fetch('https://6449df59a8370fb3213feeda.mockapi.io/list/', {
+    method: 'POST',
+    headers: {'content-type':'application/json'},
+    body: JSON.stringify({ name }),
+    next: { revalidate: 0 }
+  })
+  
+  reSetName();
+  refresh();
+}
+
 export default function AddNewTodo() {
+  const router = useRouter();
+  const [name, setName] = useState("");
+  
+  const reSetName = () => setName("");
+  
   return (
     <div>
-      <input type="text"/>
-      <button>Add</button>
+      <input type="text" onChange={(e) => setName(e.target.value)}/>
+      <button onClick={() => addTodo(name, router.refresh, reSetName)}>
+        Add
+      </button>
     </div>
   )
 }
